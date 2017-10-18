2328367fafd5cd47cd9c69faaf0c76f2de672e0f
nagaimidia <nagaimidia@gmail.com>
2017-10-18 16:58:11 -0200

    Initial Commit

---
+++ GeradorLink.php
@@ -0,0 +1,188 @@
+<?php
+##########################Criado por Jose Robson#########################################################
+##########################ATUALIZAÇÃO 23/07/2013#########################################################
+//Update 22/06/2011-------------------------------------------------------------------------
+class GeradorLink{
+		  //Troca valor por outro
+		  protected function trocaPor($texto,$palavra,$trocaPor){
+		  return ereg_replace($palavra, $trocaPor, $texto);}
+		
+		  //Remover acentos sem utf8
+		  public function removeAcentos($Msg){
+		  $a = array(
+					'/[ÂÀÁÄÃ]/'=>'A',
+				  '/[âãàáä]/'=>'a',
+					'/[ÊÈÉË]/'=>'E',
+				   '/[êèéë]/'=>'e',
+					 '/[ÎÍÌÏ]/'=>'I',
+					 '/[îíìï]/'=>'i',
+					 '/[ÔÕÒÓÖ]/'=>'O',
+					'/[ôõòóö]/'=>'o',
+					'/[ÛÙÚÜ]/'=>'U',
+					'/[ûúùü]/'=>'u',
+					'/ç/'=>'c',
+					'/Ç/'=> 'C');
+			// Tira o acento pela chave do array                        
+			 return preg_replace(array_keys($a), array_values($a), $Msg);
+			 unset($a); unset($Msg);
+		 }
+		 
+		 //Nessa parte remove conteúdo com codificação UTF8
+		 private function removeAcentosUtf8($str, $enc = "UTF-8"){
+			$acentos = array(
+			"A" => "/&Agrave;|&Aacute;|&Acirc;|&Atilde;|&Auml;|&Aring;/",
+			"a" => "/&agrave;|&aacute;|&acirc;|&atilde;|&auml;|&aring;/",
+			"C" => "/&Ccedil;/",
+			"c" => "/&ccedil;/",
+			"E" => "/&Egrave;|&Eacute;|&Ecirc;|&Euml;/",
+			"e" => "/&egrave;|&eacute;|&ecirc;|&euml;/",
+			"I" => "/&Igrave;|&Iacute;|&Icirc;|&Iuml;/",
+			"i" => "/&igrave;|&iacute;|&icirc;|&iuml;/",
+			"N" => "/&Ntilde;/",
+			"n" => "/&ntilde;/",
+			"O" => "/&Ograve;|&Oacute;|&Ocirc;|&Otilde;|&Ouml;/",
+			"o" => "/&ograve;|&oacute;|&ocirc;|&otilde;|&ouml;/",
+			"U" => "/&Ugrave;|&Uacute;|&Ucirc;|&Uuml;/",
+			"u" => "/&ugrave;|&uacute;|&ucirc;|&uuml;/",
+			"Y" => "/&Yacute;/",
+			"y" => "/&yacute;|&yuml;/",
+			"a." => "/&ordf;/",
+			"o." => "/&ordm;/");
+			
+			return preg_replace($acentos,
+			array_keys($acentos),
+			htmlentities($str,ENT_NOQUOTES, $enc));
+		}
+		 
+		
+	 	//funcão para eliminar todo html
+		public function eliminaHtml($document){
+			$search = array('@<script[^>]*?>.*?</script>@si',  // Strip out javascript
+						   '@<[\/\!]*?[^<>]*?>@si',            // Strip out HTML tags
+						   '@<style[^>]*?>.*?</style>@siU',    // Strip style tags properly
+						   '@<![\s\S]*?--[ \t\n\r]*>@'         // Strip multi-line comments including CDATA
+							);
+			$document = trim($document);
+			$text = preg_replace($search, '', $document);
+			// para garantir
+			$text = strip_tags($text);
+			return $text;
+			unset($text);
+			unset($document);
+		}
+		
+		//Removendo caracteres tipo html
+		private function cEspeciaisHtml($palavra)
+		{
+				$palavra = trim($palavra);
+				//variavel com caracter invalido para a string requerida
+				$vowels = array(chr(130),"&sbquo;",chr(131),"&fnof",chr(132),"&bdquo;",chr(133),"&hellip;",chr(134),"&dagger;",chr(135),"&Dagger;",chr(136),"&circ;",chr(137),"&permil;",chr(138),"&Scaron;",chr(139),"&lsaquo;",chr(140),"&OElig;",chr(145),"&lsquo;",chr(146),"&rsquo;",chr(147),"&ldquo;",chr(148),"&rdquo;",chr(149),"&bull;",chr(150),"&ndash;",chr(151),"&mdash;",chr(152),"&tilde;",chr(153),"&trade;",chr(154),"&scaron;",chr(155),"&rsaquo;",chr(156),"&oelig;",chr(159),"&Yuml;","&euro;","&quot;","&amp;","&lt;","&gt;","&nbsp;","&copy;","&curren;","&deg;","&divide;","&iquest;","&laquo;","&not;","&middot;","&macr;","&micro;","&acute;");
+				//tirando "
+				$texto = str_replace($vowels, "", "$palavra");
+				$texto = $this->trocaPor($texto,"-"," ");
+				$texto = $this->trocaPor($texto,"_"," ");
+				//echo $texto.'<br />';
+				return $texto;
+				unset($texto);
+				unset($palavra);
+				unset($vowels);
+		}
+		 
+		//Removendo caracteres especiais
+		private function cEspeciais($palavra)
+		{
+				$palavra = trim($palavra);
+				//retira possíveis entradas html
+				$palavra = $this->cEspeciaisHtml($palavra);
+				//variavel com caracter invalido para a string requerida
+				$vowels = array("!","?","(",")","[","]","{","}",":","|","\\",",",";","`",".","'","\"","''","'",",","","^","~","&","$","%","@","+","*","/","#","=","»","«","<",">","¶","´","ß","º","¿","®","ñ","Ñ","Ø","×","ƒ","Æ","æ","¥","•","¸","¹","³","²",".","?",":","=","-","+","*","?","?","?","?");
+				
+				//tirando "
+				$texto = str_replace($vowels, "", "$palavra");
+				$texto = $this->trocaPor($texto,"-"," ");
+				$texto = $this->trocaPor($texto,"_"," ");
+				//echo $texto.'<br />';
+				return $texto;
+				unset($texto);
+				unset($palavra);
+				unset($vowels);
+		}
+		
+		
+		//Transforma caracteres especiais e espacos em tracos
+		private function tiraCaracterTraco($palavra)
+		{
+				$palavra = $this->cEspeciais(trim($palavra));
+				//variavel com caracter invalido para a string requerida
+				$texto = $this->trocaPor(trim($palavra),"     "," ");
+				$texto = $this->trocaPor(trim($palavra),"    "," ");
+				$texto = $this->trocaPor(trim($texto),"   "," ");
+				$texto = $this->trocaPor(trim($texto),"  "," ");
+				$texto = $this->trocaPor(trim($texto),"\r"," ");
+				$texto = $this->trocaPor(trim($texto),"\n"," ");
+				$texto = $this->trocaPor(trim($texto),"\r\n"," ");
+				$texto = $this->trocaPor(trim($texto),"\t"," ");
+				$texto = preg_replace("/(<br.*?>)/i","", $texto);
+				$texto = $this->trocaPor($texto," ","-");
+				return $texto;
+				unset($texto);
+				unset($palavra);
+		}
+		
+		//Transforma caracteres especiais e espacos em underline
+		private function tiraCaracterUnderline($palavra)
+		{
+				$palavra = $this->cEspeciais(trim($palavra));
+				//variavel com caracter invalido para a string requerida
+				$texto = $this->trocaPor(trim($palavra),"     "," ");
+				$texto = $this->trocaPor(trim($palavra),"    "," ");
+				$texto = $this->trocaPor(trim($texto),"   "," ");
+				$texto = $this->trocaPor(trim($texto),"  "," ");
+				$texto = $this->trocaPor($texto," ","_");
+				return $texto;
+				unset($texto);
+				unset($palavra);
+		}
+		
+		//Gera link com traço
+		public function gerarLinkTraco($link)
+		{ return strtolower(trim($this->tiraCaracterTraco($this->removeAcentos($this->eliminaHtml(trim($link))))));}
+		
+		//Gera link com underline
+		public function gerarLinkUnderline($link)
+		{ return strtolower(trim($this->tiraCaracterUnderline($this->removeAcentos($this->eliminaHtml(trim($link))))));}
+		
+		//Gera link com traço com utf8
+		public function gerarLinkTracoUtf8($link)
+		{ return strtolower(trim($this->tiraCaracterTraco($this->removeAcentosUtf8($this->eliminaHtml(trim($link))))));}
+		
+		//Gera link com underline com utf8
+		public function gerarLinkUnderlineUtf8($link)
+		{ return strtolower(trim($this->tiraCaracterUnderline($this->removeAcentosUtf8($this->eliminaHtml(trim($link))))));}
+		
+}//fim Classe
+
+
+/*
+//Funcionamento para gerar_link.php
+###FUNCIONA ASSIM....
+//Testando a classe de geraçao de links
+$OB = new GeradorLink;
+$textinho11 = "?MARIA « TAL »/? &euro; &amp; &&& #! ``~()TAL VOCê é isso ai fautão é legalzinho É muito ísso ACHA \"\" '' LEGAL?? OlÁ mundo CABUlóSÀO SANSão &gt; &lt; &euro; é &acute; & &nbsp;  um &copy; = animalzÃOß ínigualável éssa úva óvo &quot; &amp;? SÀO São ` PÃO ";
+
+$textinho11utf = utf8_encode("?MARIA « TAL »/? &euro; &amp; &&& #! ``~()TAL VOCê é isso ai fautão é legalzinho É muito ísso ACHA \"\" '' LEGAL?? OlÁ mundo = SÀO São ` PÃO CABUlóSÀO SANSão &gt; &lt; &euro; é &acute; & &nbsp;  um &copy; animalzÃOß ínigualável éssa úva óvo &quot; &amp;?");
+
+
+//Em açao
+$textinho = $OB->gerarLinkTraco($textinho11);
+$textinho11 = $OB->gerarLinkUnderline($textinho11);
+$textinho2 = $OB->gerarLinkTracoUtf8($textinho11utf);
+$textinho22 = $OB->gerarLinkUnderlineUtf8($textinho11utf);
+
+
+echo $textinho;
+echo '<br><br>'.$textinho11;
+echo '<br><br>'.$textinho2;
+echo '<br><br>'.$textinho22;
+*/
+?>
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-* Meu primeiro repositorio
+/// Desenvolvido por José Robson /2017 - NagaiMidia.com.br
+A função desta classe é remover acentos e normalizar um link para web
+
+Exemplo de entrada:
+
+$entrada          = "O Universo é muito estranho?";
+$saida            = "o-universo-e-muito-estranho";
+$saidaAlternativa = "o_universo_e_muito_estranho"; 
